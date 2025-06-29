67d22810830919dc0f75ef9b609cfa8c3687b97f
Gabriel Toledo Melo <gabriel.t.melo6@aluno.senai.br>
2025-06-29 19:19:33 -0300

    Create cadastro.js

---
+++ static/js/cadastro.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById('cadastroForm');
+
+  form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const data = {
+      nome: document.getElementById('nome').value,
+      email: document.getElementById('email').value,
+      nascimento: document.getElementById('nascimento').value,
+      cargo: document.getElementById('cargo').value,
+      setor: document.getElementById('setor').value,
+      empresa: document.getElementById('empresa').value,
+      ramo: document.getElementById('ramo').value,
+      categoria: document.querySelector('select[name="categoria"]').value,
+      senha: document.getElementById('senha').value
+    };
+
+    try {
+      const response = await fetch('http://127.0.0.1:5000/cadastrar_usuario', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      const result = await response.json();
+      alert("Usuário cadastrado com sucesso!");
+      console.log(result);
+
+      window.location.href = "../templates/login.html";
+
+    } catch (error) {
+      alert("Erro ao cadastrar usuário.");
+      console.error("Erro:", error);
+    }
+  });
+});
